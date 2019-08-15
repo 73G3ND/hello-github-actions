8254cc99c5d3a46a691b351b48bd52f473d4f0d5
73G3ND <54087938+73G3ND@users.noreply.github.com>
2019-08-14 19:12:39 -0500

    update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh -l
-git clone https://github.com/73G3ND/hello-github-actions.git
-cd hello-github-actions
-git checkout first-action
+
 sh -c "echo Hello world my name is $ATTEM"
-chmod +x action-a/entrypoint.sh
-git add action-a/entrypoint.sh
-git commit -m "make entrypoint script executable"
-git push
+chmod +x action-a/entrypoint.sh\
