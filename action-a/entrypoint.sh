b08842884323c862a7645cd0d1fd1fe9c4b0f840
73G3ND <54087938+73G3ND@users.noreply.github.com>
2019-08-14 19:56:38 -0500

    git push update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,2 +1,7 @@
 sh -c "echo Hello world my name is $MY_NAME"
 chmod +x action-a/entrypoint.sh
+git clone https://github.com/73G3ND/hello-github-actions.git
+cd hello-github-actions
+git checkout first-action
+git add action-a/entrypoint.sh
+git commit -m "make entrypoint script executable"
